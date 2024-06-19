b753e4bcfb64913b7a3f9db1964d16889fee3f1d
David Erenfeld <1020dudu@gmail.com>
2024-06-19 20:52:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "ace-builds": path.resolve(__dirname, "./node_modules/ace-builds"),
+    },
+  },
+  base: "/node_modules/ace-builds/src-noconflict/",
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+        assetFileNames: "static/assets/[name]-[hash].[ext]",
+      },
+    },
+  },
+});
