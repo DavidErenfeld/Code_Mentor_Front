3bedff5610f31380a73ba7a56b8e21b41454eecf
David Erenfeld <1020dudu@gmail.com>
2024-06-19 20:41:25 +0300

    Update netlify.toml

--- src/Components/CodeEditor/index.jsx
+++ src/Components/CodeEditor/index.jsx
@@ -2,7 +2,6 @@ import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-dracula";
 import "ace-builds/src-noconflict/ext-language_tools";
-import "ace-builds/src-noconflict/worker-javascript";
 import ace from "ace-builds/src-noconflict/ace";
 ace.config.set("basePath", "/node_modules/ace-builds/src-noconflict");
 import "./style.css";
